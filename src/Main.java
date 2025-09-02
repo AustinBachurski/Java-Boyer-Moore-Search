8205725b1dda105bc8d45f26018dc8a665ead3ca
abachurski <austinbachurski@gmail.com>
2025-09-02 14:32:06 -0600

    Add comments for assignment.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import BoyerMooreSearch.Demo.BoyerMooreSearchDemo;
 public class Main {
     // Program entry point.
     public static void main(String[] args) {
+        // Create the demo object with the path to the searchable text.
         BoyerMooreSearchDemo demo = new BoyerMooreSearchDemo("../../input.txt");
 
+        // Begin the demo.
         demo.start();
     }
 }
-
