35039d2c6922fa78d634696da6d89010926d690b
Austin Bachurski <austinbachurski@gmail.com>
2025-09-01 23:35:04 -0600

    Stopping for the weekend.

--- src/BoyerMooreSearch/BoyerMooreSearch.java
+++ src/BoyerMooreSearch/BoyerMooreSearch.java
@@ -1,13 +1,25 @@
 package BoyerMooreSearch;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BoyerMooreSearch {
+
+    private Map<Character, Integer> letters = new HashMap<>();
+
+
     public ArrayList<Integer> findAllMatches(char[] text, String pattern) {
         ArrayList<Integer> results = new ArrayList<>();
 
 
         return results;
+    }
+
+    private void parsePattern(String pattern) {
+        for (Character c : pattern.toCharArray()) {
+
+        }
     }
 }
 
